4a9d9578a30ad71e457821f68434b172de1d54bd
Seungjae <seungjaelim@kaist.ac.kr>
2023-04-30 00:16:01 +0900

    feat: Add transformer model

---
+++ models/recsys/user_agent_transformer.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hidden_dim, num_heads):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+        self.head_dim = hidden_dim // num_heads
+
+        self.fc_q = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_k = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_v = nn.Linear(hidden_dim, hidden_dim)
+
+        self.fc_o = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.shape[0]
+
+        Q = self.fc_q(query)
+        K = self.fc_k(key)
+        V = self.fc_v(value)
+
+        Q = Q.view(batch_size, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+
+        energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.head_dim ** 0.5
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, -1e10)
+
+        attention = torch.softmax(energy, dim=-1)
+        x = torch.matmul(attention, V)
+        x = x.permute(0, 2, 1, 3).contiguous()
+        x = x.view(batch_size, -1, self.hidden_dim)
+
+        x = self.fc_o(x)
+        return x, attention
+
+
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hidden_dim, pf_dim, dropout):
+        super().__init__()
+        self.fc_1 = nn.Linear(hidden_dim, pf_dim)
+        self.fc_2 = nn.Linear(pf_dim, hidden_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.dropout(torch.relu(self.fc_1(x)))
+        x = self.fc_2(x)
+        return x
+
+
+class UserRealEstateTransformerLayer(nn.Module):
+    def __init__(self, hidden_dim, num_heads, pf_dim, dropout):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.ff_layer_norm = nn.LayerNorm(hidden_dim)
+        self.self_attention = MultiHeadAttentionLayer(hidden_dim, num_heads)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hidden_dim, pf_dim, dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src, src_mask):
+        # self attention
+        _src, _ = self.self_attention(src, src, src, src_mask)
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+
+        # positionwise feedforward
+        _src = self.positionwise_feedforward(src)
+        src = self.ff_layer_norm(src + self.dropout(_src))
+
+        return src
+    
+class UserAgentTransformerLayer(nn.Module):
+    def __init__(self, hidden_dim, num_heads, pf_dim, dropout):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.ff_layer_norm = nn.LayerNorm(hidden_dim)
+        self.self_attention = MultiHeadAttentionLayer(hidden_dim, num_heads)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hidden_dim, pf_dim, dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src, src_mask):
+        # self attention
+        _src, _ = self.self_attention(src, src, src, src_mask)
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+
+        # positionwise feedforward
+        _src = self.positionwise_feedforward(src)
+        src = self.ff_layer_norm(src + self.dropout(_src))
+
+        return src
+
+class UserAgentTransformer(nn.Module):
+    def __init__(self, num_user_features, num_agent_features, hidden_dim, num_heads, num_layers, dropout):
+        super().__init__()
+        self.fc_user = nn.Linear(num_user_features, hidden_dim)
+        self.fc_agent = nn.Linear(num_agent_features, hidden_dim)
+        self.transformer_layers = nn.ModuleList([UserAgentTransformerLayer(hidden_dim, num_heads, hidden_dim * 4, dropout) for _ in range(num_layers)])
+        self.fc_out = nn.Linear(hidden_dim, 1)
+
+    def forward(self, user_features, agent_features, src_mask=None):
+        user_emb = self.fc_user(user_features)
+        agent_emb = self.fc_agent(agent_features)
+        src = user_emb + agent_emb
+
+        for layer in self.transformer_layers:
+            src = layer(src, src_mask)
+
+        scores = self.fc_out(src).squeeze(1)
+        return scores
